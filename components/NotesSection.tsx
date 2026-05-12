93fd1076c3252b4b1778a1c768011bc53e1bd108
vasiliki <vasiliki.panagi@nottingham.ac.uk>
2026-05-12 16:14:46 +0100

    add number of results in card

--- components/NotesSection.tsx
+++ components/NotesSection.tsx
@@ -26,11 +26,11 @@ export default async function NotesSection({
   const totalPages = Math.ceil(total / pageSize);
   return (
     <div className="p-4 space-y-4">
-      <div className="text-sm text-muted-foreground">{total} articles</div>
       <NotesList
         relatedConcepts={relatedConcepts}
         notes={notes}
         conceptDetails={conceptDetails}
+        total={total}
       />
       <NotesPagination page={page} totalPages={totalPages} />
     </div>
